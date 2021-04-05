e5e5be0c3dc8ed0ace8d4d04ecf81cd8bbb4721d
Aïman <47420032+Aiman75@users.noreply.github.com>
2021-04-05 17:53:30 +0200

    Update main.js

--- QUOTES/js/main.js
+++ QUOTES/js/main.js
@@ -1,10 +1,40 @@
-const burgerMenu = document.getElementById("burger");
-const navbarMenu = document.getElementById("menu");
+const burgerMenuOne = document.getElementById("burgerOne");
+const burgerMenuTwo = document.getElementById("burgerTwo");
+const burgerMenuThree = document.getElementById("burgerThree");
+const burgerMenuFour = document.getElementById("burgerFour");
+const burgerMenuFive = document.getElementById("burgerFive");
+const navbarMenuOne = document.getElementById("menuOne");
+const navbarMenuTwo = document.getElementById("menuTwo");
+const navbarMenuThree = document.getElementById("menuThree");
+const navbarMenuFour = document.getElementById("menuFour");
+const navbarMenuFive = document.getElementById("menuFive");
 const header = document.getElementById("header");
+const headerTwo = document.getElementById("headerTwo");
+const headerThree = document.getElementById("headerThree");
+const headerFour = document.getElementById("headerFour");
+const headerFive = document.getElementById("headerFive");
 
 // Responsive Navbar Toggle
-burgerMenu.addEventListener("click", function () {
-	navbarMenu.classList.toggle("active");
-	burgerMenu.classList.toggle("active");
-	header.classList.toggle("active");
+burgerMenuOne.addEventListener("click", function () {
+	navbarMenuOne.classList.toggle("activeOne");
+	burgerMenuOne.classList.toggle("activeOne");
 });
+
+burgerMenuTwo.addEventListener("click", function () {
+	navbarMenuTwo.classList.toggle("activeTwo");
+	burgerMenuTwo.classList.toggle("activeTwo");
+});
+burgerMenuThree.addEventListener("click", function () {
+	navbarMenuThree.classList.toggle("activeThree");
+	burgerMenuThree.classList.toggle("activeThree");
+});
+burgerMenuFour.addEventListener("click", function () {
+	navbarMenuFour.classList.toggle("activeFour");
+	burgerMenuFour.classList.toggle("activeFour");
+});
+burgerMenuFive.addEventListener("click", function () {
+	navbarMenuFive.classList.toggle("activeFive");
+	burgerMenuFive.classList.toggle("activeFive");
+});
+// 	header.classList.toggle("active");
+// });
